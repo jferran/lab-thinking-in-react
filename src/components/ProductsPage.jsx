8ba5aa7ae97dd48716a2af30456474206ac9db96
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-27 00:14:26 +0200

    iteration 5

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -7,12 +7,13 @@ import SearchBar from './SearchBar';
 function ProductsPage() {
     const [products, setProducts] = useState(jsonData);
     const [search, setSearch] = useState('')
+    const [onlyInStock, setOnlyInStock] = useState(false)
   return (
     <div>
         <h1>IronStore</h1>
-        <SearchBar setSearch={setSearch}/>
+        <SearchBar setSearch={setSearch} setOnlyInStock={setOnlyInStock}/>
         <ProductTable 
-            products={jsonData.filter((product) => product.name.toLowerCase().includes(search.toLowerCase()))}
+            products={jsonData.filter((product) => onlyInStock ? product.inStock : true && product.name.toLowerCase().includes(search.toLowerCase()))}
 
             />
     </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
-function SearchBar({setSearch}) {
+function SearchBar({setSearch, setOnlyInStock}) {
     const[searchInput, setSearchInput] = React.useState('')
     const handleChange = (event) => {
         setSearchInput(event.target.value)
         setSearch(event.target.value)
     }
+    const handleCheckBox = (event) => {
+        setOnlyInStock(event.target.checked)
+    }
   return (
     <div>
-        <input type="text" name="search" onChange={handleChange} placeHolder="Search" value={searchInput}/>
+        <input type="text" name="search" onChange={handleChange} placeholder="Search" value={searchInput}/>
+        <input type="checkbox" name="inStockFilter" onChange={handleCheckBox}/><label htmlFor='inStockFilter'>Only show products in stock</label>
     </div>
   )
 }
